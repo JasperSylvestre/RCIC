50c6028014a006ac60824fdd529a13c6807f67a4
Jasper Sylvestre <160375345+JasperSylvestre@users.noreply.github.com>
2025-04-05 16:47:02 -0400

    Update prepare_test_images.py

--- scripts/prepare_test_images.py
+++ scripts/prepare_test_images.py
@@ -11,19 +11,12 @@ import numpy as np
 import os
 from PIL import Image
 
-# Create and enter scripts directory if not present
+# Create necessary directories and enter scripts folder
 if os.path.basename(os.getcwd()) != 'scripts':
-  if not os.path.exists('scripts'):
-    os.makedirs('scripts')
+  os.makedirs('scripts', exist_ok=True)
   os.chdir('scripts')
-
-# Create models directory
-if not os.path.exists('../models'):
-  os.makedirs('../models')
-
-# Create data/test directory
-if not os.path.exists('../data/test'):
-  os.makedirs('../data/test')
+os.makedirs('../models', exist_ok=True)
+os.makedirs('../data/test/test_images', exist_ok=True)
 
 # Load the saved validation indices and the original dataset
 with open('../data/test/test_ds.pkl', 'rb') as f:
@@ -34,10 +27,6 @@ imagenet_mean = np.array([0.485, 0.456, 0.406])
 imagenet_std = np.array([0.229, 0.224, 0.225])
 
 original_ds = (test_ds * imagenet_std[:, None, None]) + imagenet_mean[:, None, None]
-
-# Define the output directory
-output_dir = '../data/test/test_images'
-os.makedirs(output_dir, exist_ok=True)
 
 # Loop through the array and save each image as a JPG
 for i in range(original_ds.shape[0]):
